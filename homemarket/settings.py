e16857fda3e441b796c4094e38193a8adadd5f8a
Donmo-K <lislornguiekou08@gmail.com>
2026-04-02 12:31:48 +0100

    fix database config

--- homemarket/settings.py
+++ homemarket/settings.py
@@ -63,10 +63,11 @@ WSGI_APPLICATION = 'homemarket.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 import dj_database_url
+import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
         conn_max_age=600,
     )
 }
