2d59d47867f8e1327f04c0dfaf5a78d8a5fabcf8
Donmo-K <lislornguiekou08@gmail.com>
2026-04-03 07:09:23 +0100

    fix superuser script

---
+++ create_superuser.py
@@ -0,0 +1,23 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homemarket.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+try:
+    if not User.objects.filter(email="admin@homemarket.com").exists():
+        user = User(
+            email="admin@homemarket.com",
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+        )
+        user.set_password("Admin2026")
+        user.save()
+    else:
+        print("Superuser existe deja.")
+except Exception as e:
+    print(f"Erreur: {e}")
